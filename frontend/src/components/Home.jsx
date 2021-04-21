00a0ac569f068ef2949af037fef8c0cbd6824ebe
junki Kim <kjgcde@gmail.com>
2021-04-21 17:41:31 +0900

    Word cloud 기능 추가

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './Header';
+import WordCloud from './WordCloud';
 
 import '../assets/Home.css';
 
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <div className="home">
       <Header />
+      <WordCloud />
     </div>
   );
 };
