65825cac4389cf138ebb1c2ae615cccaed0c0f51
junki Kim <kjgcde@gmail.com>
2021-04-21 17:38:23 +0900

    Header CSS 고도화

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,30 +3,28 @@ import '../assets/Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <h2>Kimjunki</h2>
-        <nav>
-          <ul>
-            <li>
-              <a href="/#" className="test">
-                Introduce
-              </a>
-            </li>
-            <li>
-              <a href="/#">Skils</a>
-            </li>
-            <li>
-              <a href="/#">Experience</a>
-            </li>
-            <li>
-              <a href="/#">Contact Me</a>
-            </li>
-          </ul>
-        </nav>
-        <hr className="contour" />
-      </div>
-    </header>
+    <div className="header">
+      <h2>Kimjunki</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="/#" className="test">
+              Introduce
+            </a>
+          </li>
+          <li>
+            <a href="/#">Skils</a>
+          </li>
+          <li>
+            <a href="/#">Experience</a>
+          </li>
+          <li>
+            <a href="/#">Contact Me</a>
+          </li>
+        </ul>
+      </nav>
+      <hr className="contour" />
+    </div>
   );
 };
 
