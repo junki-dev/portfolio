1a1ae55e271e64c27d1d231abbf89dadbfa419b3
Junki Kim <kjgcde@gmail.com>
2021-06-26 11:55:51 +0900

    content header 추가 및 컴포넌트화

---
+++ frontend/src/components/HR.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HR = () => {
+  const Hr = styled.hr`
+    height: 1px;
+    background-color: #eee;
+    border: none;
+  `;
+
+  return <Hr />;
+};
+export default HR;
