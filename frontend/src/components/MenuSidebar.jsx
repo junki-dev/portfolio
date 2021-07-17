552bca45d843e7e1c7b5d38617c78905c2a39114
Junki Kim <kjgcde@gmail.com>
2021-07-17 10:00:46 +0900

    hr 컴포넌트 추가 및 메뉴 슬라이드 수정

--- frontend/src/components/MenuSidebar.jsx
+++ frontend/src/components/MenuSidebar.jsx
@@ -5,20 +5,33 @@ import '../assets/MenuSidebar.css';
 import { Link } from 'react-router-dom';
 
 const MenuSidebar = () => {
+  // const [selected, setSelected] = useState(1);
+
+  // const handleSelected = item => {
+  //   setSelected(item);
+  //   // eslint-disable-next-line no-console
+  //   console.log(selected);
+  //   console.log(`들어옴`)
+  // };
+
   return (
     <ProSidebar className="sidebar-menu">
       <Menu>
-        <MenuItem prefix="01." className="menu-item">
-          <Link to="/about">ABOUT</Link>
+        <MenuItem prefix="01.">
+          ABOUT
+          <Link to="/about" />
         </MenuItem>
-        <MenuItem prefix="02." className="menu-item">
-          <Link to="/work">WORK</Link>
+        <MenuItem prefix="02.">
+          WORK
+          <Link to="/work" />
         </MenuItem>
-        <MenuItem prefix="03." className="menu-item">
-          <Link to="/skill">SKILL</Link>
+        <MenuItem prefix="03.">
+          SKILL
+          <Link to="/skill" />
         </MenuItem>
-        <MenuItem prefix="04." className="menu-item">
-          <Link to="/lab">LAB</Link>
+        <MenuItem prefix="04.">
+          LAB
+          <Link to="/lab" />
         </MenuItem>
       </Menu>
     </ProSidebar>
