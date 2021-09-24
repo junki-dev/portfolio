42d8752fbe36a9831bb9ad2aa3f9057a4484f5e3
Junki Kim <kjgcde@gmail.com>
2021-09-24 20:56:52 +0900

    fix: front-end sidebar styled component 전환, skill/work 메뉴 링크 수정

--- frontend/src/components/Menu/MenuSidebar.jsx
+++ frontend/src/components/Menu/MenuSidebar.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-import '../../assets/MenuSidebar.css';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const MenuContainer = styled(ProSidebar)`
+  width: 23%;
+  height: 100vh;
+  margin-left: 0px;
+  min-width: 3%;
+
+  .pro-sidebar-inner {
+    width: 100%;
+    padding-top: 4vh;
+    background-color: #fff;
+    border: 1px solid #eee;
+  }
+
+  .prefix-wrapper {
+    font-size: 2em;
+    font-family: BigShouldersRegular;
+  }
+
+  .pro-inner-item {
+    width: 100%;
+    height: 23vh;
+
+    .pro-item-content {
+      width: 100%;
+      height: 100%;
+      font-size: 8em;
+      font-family: BigShouldersBold;
+    }
+  }
+`;
 
 const MenuSidebar = () => {
   return (
-    <ProSidebar className="sidebar-menu">
+    <MenuContainer>
       <Menu>
         <MenuItem prefix="01.">
           ABOUT
@@ -25,7 +56,7 @@ const MenuSidebar = () => {
           <Link to="/lab" />
         </MenuItem>
       </Menu>
-    </ProSidebar>
+    </MenuContainer>
   );
 };
 
