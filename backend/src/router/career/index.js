811f2d4c6dff65177736c2ae1c8941919d5b666e
Junki Kim <kjgcde@gmail.com>
2021-09-25 18:50:04 +0900

    feat: skill 조회 및 상세 조회 api 개발

--- backend/src/router/career/index.js
+++ backend/src/router/career/index.js
@@ -4,6 +4,6 @@ const careerController = require('../../controller/career');
 const router = express.Router();
 
 router.get('/', careerController.getCareerList);
-router.get('/getCareerByData', careerController.getCareerDetail);
+router.get('/getCareerById', careerController.getCareerDetail);
 
 module.exports = router;
--- backend/src/router/skill/index.js
+++ backend/src/router/skill/index.js
@@ -4,5 +4,6 @@ const skillController = require('../../controller/skill');
 const router = express.Router();
 
 router.get('/', skillController.getSkillMenuList);
+router.get('/getSkillByName', skillController.getSkillDetail);
 
 module.exports = router;
