aae9bb335af5b96c8f9f1646b8f07f54d89e9766
Junki Kim <kjgcde@gmail.com>
2021-10-06 21:44:53 +0900

    feat: mongodb 연결 방식 수정

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,15 +10,19 @@ import skillRouter from './router/skill/index.js'; // career 라우터
 logger.debug(`# JUNKI PORTFOLIO BACKEND SERVER STARTing...`);
 const app = express(); // 애플리케이션 미들웨어 바인딩
 const __dirname = path.resolve(); // 디렉토리의 절대경로를 반환
-const { PORT, MONGO_URI } = process.env; // 서버 포트 지정
+const port = process.env.PORT; // 서버 포트 지정
 
 /* middleware 설정 */
 /// CORS(Cross Origin Resource Sharing) 에러 해결을 위한 미들웨어(react 서버를 별도로 사용하기 때문) 삭제 예정
 app.use(cors());
 
 // MongoDB 연결
+const connectionInfo = `${process.env.USER_ID}:${process.env.USER_PWD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(
+    `mongodb://${connectionInfo}?authSource=admin&authMechanism=SCRAM-SHA-1`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
   .then(() => logger.info('Successfully connected to mongodb'))
   .catch(e => logger.error(e));
 
@@ -28,4 +32,4 @@ app.use('/career', careerRouter); // 경력 관련 routing
 app.use('/skill', skillRouter); // 기술 관련 routing
 
 // 서버 구동
-app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
+app.listen(port, () => logger.info(`Server listening on port ${port}`));
