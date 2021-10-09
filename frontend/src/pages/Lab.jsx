cf925b95a769db4d5545a76d3da12e4821964087
Junki Kim <kjgcde@gmail.com>
2021-10-10 03:45:59 +0900

    feat: Lab 페이지 추가

--- frontend/src/pages/Lab.jsx
+++ frontend/src/pages/Lab.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import Sidebar from '../components/Menu/Sidebar';
 
-// Component style
-const Section = styled.div`
+// Lab 메뉴 style
+const LabContiner = styled.div`
   display: flex;
   width: 100%;
   margin: 0 auto;
   color: #000;
 `;
 
+// Lab 영역 style
+const LabSection = styled.div`
+  padding: 5.8rem 8rem 4rem 8rem;
+  width: 70%;
+`;
+
+// 제목 style
+const Title = styled.span`
+  display: inline-block;
+  font-size: 6em;
+  letter-spacing: -1px;
+  font-family: NanumBarunpen;
+  white-space: nowrap;
+`;
+
+// 설명 style
+const Description = styled.span`
+  display: inline-block;
+  font-size: 2.3em;
+  letter-spacing: -1px;
+  font-family: NanumBarunpen;
+  white-space: nowrap;
+`;
+
 const Lab = () => {
   return (
-    <Section>
+    <LabContiner>
       <Sidebar />
-      <span>lab 페이지입니다.</span>
-    </Section>
+      <LabSection>
+        <Title>Laboratory _ Comming soon...</Title>
+        <br />
+        <Description>
+          알고 있는 것을 더 섬세하고 세련되게, 모르는 것은 경험을 통해 배우기
+          위한 곳입니다.
+        </Description>
+        <br />
+      </LabSection>
+    </LabContiner>
   );
 };
 
